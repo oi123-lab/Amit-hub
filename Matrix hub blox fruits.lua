8ca0d5e46846db43981504cf4666292e38d83e26
oi123-lab <ggggprowar@gmail.com>
2025-05-09 14:14:28 -0300

    Matrix hub blox fruits.lua

--- Matrix hub blox fruits.lua
+++ Matrix hub blox fruits.lua
@@ -3,7 +3,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 ----------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
-    Title = "ReworkX Hub | Blox Fruits",
+    Title = "Matrix Hub : By Not Legitty Matrix Community| Blox Fruits",
     SubTitle = "V1",
     TabWidth = 160,
     Size = UDim2.fromOffset(500, 320),
